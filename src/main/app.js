ddd1d3137c0e35223176cda0828e2e7f71aace80
Billy He <billy@metalabdesign.com>
2018-12-26 13:00:27 -0800

    Move some web content references handling code into MainWindow class

--- src/main/app.js
+++ src/main/app.js
@@ -1,28 +1,19 @@
 import {
   app as electronApp,
   ipcMain as electronIpcMain,
-  shell as electronShell,
   Menu as ElectronMenu
 } from "electron";
 
 import { join as joinPath } from "path";
 import uuidv4 from "uuid/v4";
 import flow from "lodash/fp/flow";
-import get from "lodash/fp/get";
-import negate from "lodash/fp/negate";
 import getOr from "lodash/fp/getOr";
 import values from "lodash/fp/values";
-import filter from "lodash/fp/filter";
-import size from "lodash/fp/size";
-import lte from "lodash/fp/lte";
 import isEmpty from "lodash/fp/isEmpty";
 import map from "lodash/fp/map";
 import sum from "lodash/fp/sum";
 import debounce from "lodash/fp/debounce";
-import once from "lodash/fp/once";
 import findIndex from "lodash/fp/findIndex";
-import isNumber from "lodash/fp/isNumber";
-import isFinite from "lodash/fp/isFinite";
 
 import LocalSettings from "./local-settings";
 import RemoteSettings from "./remote-settings";
@@ -30,16 +21,12 @@ import Preloads from "./preloads";
 import Menus from "./menus";
 import MainWindow from "./main-window";
 
-import { getInternalUrlChecker } from "./url-checker";
-
-const isFiniteNumber = (value) => (isNumber(value) && isFinite(value));
 const getReplier = (sender) => (...args) => sender.send(...args);
 
 class App {
   constructor() {
     this.mainWindow = null;
     this.allSites = {};
-    this.allWebContents = {};
 
     this.localSettings = new LocalSettings({
       settingsFilePath: joinPath(electronApp.getPath("userData"), "settings.json")
@@ -54,9 +41,7 @@ class App {
     this.handleElectronAppReady = this.handleElectronAppReady.bind(this);
     this.handleElectronAppActivate = this.handleElectronAppActivate.bind(this);
     this.handleElectronAppWindowAllClosed = this.handleElectronAppWindowAllClosed.bind(this);
-    this.handleElectronAppWebContentsCreated = this.handleElectronAppWebContentsCreated.bind(this);
 
-    this.handleElectronIpcMainWebContentsCreated = this.handleElectronIpcMainWebContentsCreated.bind(this);
     this.handleElectronIpcMainSiteUnreadCountChanged = this.handleElectronIpcMainSiteUnreadCountChanged.bind(this);
     this.handleElectronIpcMainSiteActivated = this.handleElectronIpcMainSiteActivated.bind(this);
     this.handleElectronIpcMainAppDidMount = this.handleElectronIpcMainAppDidMount.bind(this);
@@ -64,18 +49,13 @@ class App {
     this.handleMainWindowClose = this.handleMainWindowClose.bind(this);
     this.saveMainWindowSizeAndPosition = debounce(1000)(this.saveMainWindowSizeAndPosition.bind(this));
     this.saveActiveSiteIndex = debounce(1000)(this.saveActiveSiteIndex.bind(this));
-
-    // Use `once` because this is called from parallel `web-contents-created` handler calls.
-    this.onAllSitesWebContentsCreated = once(this.onAllSitesWebContentsCreated.bind(this));
   }
 
   start() {
     electronApp.on("ready", this.handleElectronAppReady);
     electronApp.on("activate", this.handleElectronAppActivate);
     electronApp.on("window-all-closed", this.handleElectronAppWindowAllClosed);
-    electronApp.on("web-contents-created", this.handleElectronAppWebContentsCreated);
 
-    electronIpcMain.on("web-contents-created", this.handleElectronIpcMainWebContentsCreated);
     electronIpcMain.on("site-unread-count-changed", this.handleElectronIpcMainSiteUnreadCountChanged);
     electronIpcMain.on("site-activated", this.handleElectronIpcMainSiteActivated);
     electronIpcMain.on("app-did-mount", this.handleElectronIpcMainAppDidMount);
@@ -96,53 +76,6 @@ class App {
     // TODO: Implement keep-alive for Mac OS (need to better keep-track/cleanup webContents references).
     // TODO: Implement minimize-to-tray for Windows/Linux (see https://stackoverflow.com/a/44501184).
     electronApp.quit();
-  }
-
-  // This `web-contents-created` event is fired by Electron itself when *any* WebContents object is created.
-  // Here we catch them all because we need reference to some of them and we don't know yet which one is which, but
-  // we will in a bit (See the `web-contents-created` handler from `electronIpcMain`).
-  handleElectronAppWebContentsCreated(_, webContents) {
-    this.allWebContents[webContents.id] = webContents;
-  }
-
-  handleElectronIpcMainWebContentsCreated(evt, { siteId, webContentId }) {
-    const sendReply = getReplier(evt.sender);
-
-    const site = this.allSites[siteId];
-    const webContents = this.allWebContents[webContentId];
-
-    const isUrlInternal = getInternalUrlChecker({
-      externalUrlPatterns: site.externalUrlPatterns,
-      internalUrlPatterns: site.internalUrlPatterns
-    });
-
-    webContents.on("new-window", (evt, url) => {
-      if (!isUrlInternal(url)) {
-        evt.preventDefault();
-        electronShell.openExternal(url);
-      }
-    });
-
-    webContents.on("will-navigate", (evt, url) => {
-      if (!isUrlInternal(url)) {
-        evt.preventDefault();
-        electronShell.openExternal(url);
-      }
-    });
-
-    site.webContentsReady = true;
-
-    flow([
-      values,
-      filter(negate(get("webContentsReady"))),
-      size,
-      lte(0),
-      (ready) => {
-        if (ready) {
-          this.onAllSitesWebContentsCreated({ sendReply });
-        }
-      }
-    ])(this.allSites);
   }
 
   handleElectronIpcMainSiteUnreadCountChanged(evt, { siteId, unreadCount }) {
@@ -202,7 +135,14 @@ class App {
 
   createMainWindow() {
     this.localSettings.getSettings().then(({ posX, posY, width, height }) => {
-      this.mainWindow = new MainWindow({ posX, posY, width, height });
+      this.mainWindow = new MainWindow({
+        localSettings: this.localSettings,
+        allSites: this.allSites,
+        posX,
+        posY,
+        width,
+        height
+      });
       this.mainWindow.on("closed", this.handleMainWindowClose);
       this.mainWindow.on("resize-move", this.saveMainWindowSizeAndPosition);
     });
@@ -241,26 +181,6 @@ class App {
         sendReply("add-site", { site });
       }).then(() => this.addSites({ sendReply, sites: restSites }));
     }
-  }
-
-  onAllSitesWebContentsCreated({ sendReply }) {
-    this.localSettings.getSettings().then(({ activeSiteIndex }) => {
-      if (isFiniteNumber(activeSiteIndex) && activeSiteIndex >= 0) {
-        const activeSite = flow([
-          values,
-          get(activeSiteIndex)
-        ])(this.allSites);
-
-        if (activeSite) {
-          console.log("Restore active site", activeSite.id);
-          sendReply("set-active-site-id", { activeSiteId: activeSite.id });
-        }
-      }
-    });
-
-    sendReply("set-app-states", {
-      states: { isAppReady: true }
-    });
   }
 }
 
--- src/main/main-window.js
+++ src/main/main-window.js
@@ -1,10 +1,34 @@
-import { BrowserWindow } from "electron";
+import {
+  app as electronApp,
+  ipcMain as electronIpcMain,
+  shell as electronShell,
+  BrowserWindow as ElectronBrowserWindow
+} from "electron";
+
 import EventEmitter from "events";
+
+import flow from "lodash/fp/flow";
+import values from "lodash/fp/values";
+import filter from "lodash/fp/filter";
+import negate from "lodash/fp/negate";
+import get from "lodash/fp/get";
+import size from "lodash/fp/size";
+import lte from "lodash/fp/lte";
+import isNumber from "lodash/fp/isNumber";
+import isFinite from "lodash/fp/isFinite";
+import once from "lodash/fp/once";
+
+import { getInternalUrlChecker } from "/src/main/url-checker";
 
 import Icon from "./icon";
 
+const isFiniteNumber = (value) => (isNumber(value) && isFinite(value));
+const getReplier = (sender) => (...args) => sender.send(...args);
+
 class MainWindow extends EventEmitter {
   constructor({
+    localSettings,
+    allSites,
     posX,
     posY,
     width,
@@ -12,7 +36,11 @@ class MainWindow extends EventEmitter {
   }) {
     super();
 
-    this.browserWindow = new BrowserWindow({
+    this.localSettings = localSettings;
+    this.allSites = allSites;
+    this.allWebContents = {};
+
+    this.browserWindow = new ElectronBrowserWindow({
       title: "Voll",
 
       x: posX,
@@ -39,10 +67,86 @@ class MainWindow extends EventEmitter {
     this.handleResizeMove = this.handleResizeMove.bind(this);
     this.handleClose = this.handleClose.bind(this);
 
+    this.handleElectronIpcMainWebContentsCreated = this.handleElectronIpcMainWebContentsCreated.bind(this);
+    this.handleElectronAppWebContentsCreated = this.handleElectronAppWebContentsCreated.bind(this);
+
     this.browserWindow.on("page-title-updated", this.handlePageTitleUpdated);
     this.browserWindow.on("resize", this.handleResizeMove);
     this.browserWindow.on("move", this.handleResizeMove);
     this.browserWindow.on("closed", this.handleClose);
+
+    electronApp.on("web-contents-created", this.handleElectronAppWebContentsCreated);
+    electronIpcMain.on("web-contents-created", this.handleElectronIpcMainWebContentsCreated);
+
+    // Use `once` because this is called from parallel `web-contents-created` handler calls.
+    this.onAllSitesWebContentsCreated = once(this.onAllSitesWebContentsCreated.bind(this));
+  }
+
+  // This `web-contents-created` event is fired by Electron itself when *any* WebContents object is created.
+  // Here we catch them all because we need reference to some of them and we don't know yet which one is which, but
+  // we will in a bit (See the `web-contents-created` handler from `electronIpcMain`).
+  handleElectronAppWebContentsCreated(_, webContents) {
+    this.allWebContents[webContents.id] = webContents;
+  }
+
+  handleElectronIpcMainWebContentsCreated(evt, { siteId, webContentId }) {
+    const sendReply = getReplier(evt.sender);
+
+    const site = this.allSites[siteId];
+    const webContents = this.allWebContents[webContentId];
+
+    const isUrlInternal = getInternalUrlChecker({
+      externalUrlPatterns: site.externalUrlPatterns,
+      internalUrlPatterns: site.internalUrlPatterns
+    });
+
+    webContents.on("new-window", (evt, url) => {
+      if (!isUrlInternal(url)) {
+        evt.preventDefault();
+        electronShell.openExternal(url);
+      }
+    });
+
+    webContents.on("will-navigate", (evt, url) => {
+      if (!isUrlInternal(url)) {
+        evt.preventDefault();
+        electronShell.openExternal(url);
+      }
+    });
+
+    site.webContentsReady = true;
+
+    flow([
+      values,
+      filter(negate(get("webContentsReady"))),
+      size,
+      lte(0),
+      (ready) => {
+        if (ready) {
+          this.onAllSitesWebContentsCreated({ sendReply });
+        }
+      }
+    ])(this.allSites);
+  }
+
+  onAllSitesWebContentsCreated({ sendReply }) {
+    this.localSettings.getSettings().then(({ activeSiteIndex }) => {
+      if (isFiniteNumber(activeSiteIndex) && activeSiteIndex >= 0) {
+        const activeSite = flow([
+          values,
+          get(activeSiteIndex)
+        ])(this.allSites);
+
+        if (activeSite) {
+          console.log("Restore active site", activeSite.id);
+          sendReply("set-active-site-id", { activeSiteId: activeSite.id });
+        }
+      }
+    });
+
+    sendReply("set-app-states", {
+      states: { isAppReady: true }
+    });
   }
 
   handleClose() {
